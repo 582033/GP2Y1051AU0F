d9bc247a19e3a2bd15cb644a9c6482330b6b5e6d
Yan Jiang <j582033@163.com>
2015-11-20 15:34:45 +0800

    调整自动获取数据起始位

--- run.py
+++ run.py
@@ -4,53 +4,61 @@ import time,serial
 
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial('/dev/tty.usbserial', 2400)
 ser = serial.Serial(
         port='/dev/tty.usbserial',
         baudrate=2400,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS
+        bytesize=serial.EIGHTBITS,
+        timeout = 1
 )
 #ser.open()
 #ser.write("testing")
+
+def debug():
+    byteData = ser.read(7) # read one, blocking
+    byteData += ser.read(ser.inWaiting()).encode('hex')
+    print byteData.encode('hex')
 
 def num_format(num_hex):
     num_int = int(num_hex, 16)
     return float(format(num_int, '.10f'))
 
-def show():
-    line = ser.readline().encode('hex')
-    #Dustdensity 灰尘密度
-    #Vout       输入电压
-    #004d57ffaa000a
-    A = 550  #比例系数,由用户自定义
-    Start = num_format(line [8:10])
-    Vout_h = num_format(line [10:12])
-    Vout_l = num_format(line [12:14])
-    Verf_h = num_format(line [0:2])
-    Verf_l = num_format(line [2:4])
-    Verf = num_format(line [4:6])
-    Stop = num_format(line [6:8])
+def get_vout(hex_str):
+    #0005004d52ffaa
+    #找到数据开始位aa
+    index = hex_str.find('aa')
+    #获取Vout_h索引位置
+    if index==12:
+        Vout_h_index = 0
+    else:
+        Vout_h_index = index + 2
+    #获取Vout_l索引位置
+    if Vout_h_index==12:
+        Vout_l_index = 0
+    else:
+        Vout_l_index = Vout_h_index + 2
+
+    #开始计算
+    Vout_h = num_format(hex_str [Vout_h_index:Vout_h_index+2])
+    Vout_l = num_format(hex_str [Vout_l_index:Vout_l_index+2])
 
     Vout = ((Vout_h * 256) + Vout_l) / 1024 * 5     #输入电压
+    return Vout
+
+def show():
+    byteData = ser.read(7)                          # read one, blocking
+    byteData += ser.read(ser.inWaiting()).encode('hex')
+    line = byteData.encode('hex')
+
+    A = 550                                         #比例系数,由用户自定义
+    Vout = get_vout(line)                           #输入电压
+    #Vout = ((Vout_h * 256) + Vout_l) / 1024 * 5    #输入电压
     Dustdensity = int(A * Vout)                     #灰尘密度,单位 ug/m3
     print "====================="
-    #print "Start:" + str(Start)
-    #print "Vout_h:" + str(Vout_h)
-    #print "Vout_l:" + str(Vout_l)
-    #print "Verf_h:" + str(Verf_h)
-    #print "Verf_l:" + str(Verf_l)
-    #print "Verf:" + str(Verf)
-    #print "Stop:" + str(Stop)
     print "Vout:" + str(Vout) + "V"
-    print "Dustdensity:" + str(Dustdensity) + "ug/m3"
-    #time.sleep(1)
-
-def debug():
-    line = ser.readline().encode('hex')
-    print line
-
+    print "DustDensity:" + str(Dustdensity) + "ug/m3"
+    time.sleep(1)
 
 try:
     while 1:
